88243004aa7507069e439bb600934469d4769ecb
giorgio laera <laeragiorg@gmail.com>
2025-10-29 20:46:55 +0100

    commenti creati

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("JS funzionane")
+
+
+//1. creare una variabile dove andare a salvare i valori
+let i= NaN;
+//2. creare un programma che vada a stampare i numeri da 1 a 100
+for ( i = 0; i < 101; i++){
+    console.log(i);
+
+}
+//3. verificare se un numero e divisibile  per 3 e se si stampare  'Fizz'
+
+//3. verificare se un numero e divisibile  per 5 e se si stampare  'Buzz'
+
+//3. verificare se un numero e divisibile sia per 3 che per 5 e se si stampare  'FizzBuzz'
+
+// 
